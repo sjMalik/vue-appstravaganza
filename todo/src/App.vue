37ba095d40c8e4a798dbcdd6a2e8133c60fea9ba
sjmalik <surajit.malik@qlikchain.com>
2023-06-27 08:21:16 +0530

    todo vue app done

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,27 +1,66 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo APP</h1>
-    <form>
+    <form @submit.prevent="addTodo()">
       <div class="form-group">
-        <label for="todo">New Tod</label>
-        <input type="email" class="form-control" id="todo" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="todo">New Todo</label>
+        <input v-model="newTodo" type="text" class="form-control" id="todo" aria-describedby="emailHelp" placeholder="Enter Todo Item">
+        <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-success mt-2">Submit</button>
     </form>
+    <ul class="list-group mt-3">
+      <li class="list-group-item" v-for="(todo, i) in todos">
+        <button class="btn btn-dark btn-sm" @click="markDone(todo)">Done</button>
+        <button class="btn btn-danger btn-sm" @click="removeTodo(i)">Remove</button>
+        <span :class="{isDone: todo.done}">{{ todo.title }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 todo
 <script>
 export default {
   name: 'app',
+  watch: {
+    todos: {
+      handler() {
+        localStorage.todos = JSON.stringify(this.todos);
+      },
+      deep: true
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      todos: []
+    }
+  },
+  mounted(){
+    if(localStorage.todos){
+      this.todos = JSON.parse(localStorage.todos);
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      });
+      this.newTodo = "";
+    },
+    markDone(todo){
+      todo.done = true;
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1);
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+  .isDone{
+    text-decoration: line-through;
+  }
 </style>
